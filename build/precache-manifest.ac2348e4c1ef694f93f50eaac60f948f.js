3ab4b15fbe8e542a2a61995ad0954df25dda5b50
elm-dak <mestafa.wydad66@gmail.com>
2024-01-24 14:37:49 +0100

    build

---
+++ build/precache-manifest.ac2348e4c1ef694f93f50eaac60f948f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b63f5ea3afc5ba1e844b31897c25b30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8903ce9494e843ad0a05",
+    "url": "/static/css/main.427275ce.chunk.css"
+  },
+  {
+    "revision": "f628fa6bceb7638a9cf5",
+    "url": "/static/js/2.0e5c3b65.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0e5c3b65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8903ce9494e843ad0a05",
+    "url": "/static/js/main.38d8a8f0.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
